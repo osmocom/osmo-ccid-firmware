0385ef2186ef96a2c34888b519f0d37780fabb3b
Harald Welte <laforge@osmocom.org>
2019-12-17 11:50:43 +0100

    Add --disable-libusb for libosmocore cross-compile
    libosmocore.git after I656a1a38cbb5b1f3a9145d2869d3b4d0adefcae3
    includes support for USB (and a new dependency to libusb).
    
    But there's no libusb1 in the Cortex-M4 anyway :)
    
    Change-Id: I68da7985001f4f9018995166458006ee47cdf216

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -41,7 +41,7 @@ mkdir -p "$inst/stow"
 autoreconf --install --force
 ./configure	--enable-static --prefix="$inst/stow/libosmocore" --host=arm-none-eabi \
 		--enable-embedded --disable-doxygen --disable-shared --disable-pseudotalloc \
-		--disable-libsctp --enable-external-tests \
+		--disable-libsctp --disable-libusb --enable-external-tests \
 		CFLAGS="-Os -ffunction-sections -fdata-sections -nostartfiles -nodefaultlibs -Werror -Wno-error=deprecated -Wno-error=deprecated-declarations -Wno-error=cpp -mthumb -Os -mlong-calls -g3 -mcpu=cortex-m4 -mfloat-abi=softfp -mfpu=fpv4-sp-d16 -I /home/laforge/projects/git/osmo-ccid-firmware/sysmoOCTSIM -Wno-error=format"
 make $PARALLEL_MAKE install
 make clean
