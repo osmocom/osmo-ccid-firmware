3ae39cab62e7e6fadd120c856045b4ceff4bc48b
Joachim Steiger <jsteiger@sysmocom.de>
2020-03-17 12:25:38 +0100

    switch to using 'usbrelay' and  more generic,inexpensive relay board
    Change-Id: I292e3fd8127d393838b11db7fb058eb638524abc

--- tests/sysmo-octsim/ctl_reset_target.sh
+++ tests/sysmo-octsim/ctl_reset_target.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -e
 . ./test-data
 
-#usb_rly08
+#usb_relay
 # relay1 = usbpower
 # relay2 = dfu-btn
 
 #12 off
 echo "resetting target"
-echo -n "op" > $USB_RELAY
+usbrelay BITFT_1=0 BITFT_2=0 2>/dev/null
 sleep 1
 # 1 on
-echo -n "e" > $USB_RELAY
+usbrelay BITFT_1=1 BITFT_2=0 2>/dev/null
 sleep 1
 echo "done"
--- tests/sysmo-octsim/ctl_reset_to_dfu.sh
+++ tests/sysmo-octsim/ctl_reset_to_dfu.sh
@@ -1,21 +1,21 @@
 #!/bin/sh -e
 . ./test-data
 
-#usb_rly08
+#usb_relay
 # relay1 = usbpower
 # relay2 = dfu-btn
 
 echo "resetting target to dfu"
 #12 off
-echo -n "op" > $USB_RELAY
+usbrelay BITFT_1=0 BITFT_2=0 2>/dev/null
 sleep 1
 # 2 on
-echo -n "f" > $USB_RELAY
+usbrelay BITFT_1=0 BITFT_2=1 2>/dev/null
 sleep 1
 # 1 on
-echo -n "e" > $USB_RELAY
+usbrelay BITFT_1=1 BITFT_2=1 2>/dev/null
 sleep 1
 # 2 off
-echo -n "p" > $USB_RELAY
+usbrelay BITFT_1=1 BITFT_2=0 2>/dev/null
 sleep 1
 echo "done"
