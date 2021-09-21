30c26f4ec59cf4d85d790cfe3bb547057fd96bc3
Eric Wild <ewild@sysmocom.de>
2021-09-21 03:06:01 +0200

    remove unused dmac
    Change-Id: I9646220d9864fd8ddc133da58151227c24546cc6

--- sysmoOCTSIM/atmel_start.c
+++ sysmoOCTSIM/atmel_start.c
@@ -1,19 +1,11 @@
 #include <atmel_start.h>
 
-/** Memory to memory DMA callback */
-static void M2M_DMA_complete_cb(void)
-{
-	dma_m2m_complete_flag = true;
-}
-
 /**
  * Initializes MCU, drivers and middleware in the project
  **/
 void atmel_start_init(void)
 {
 	system_init();
-	dma_memory_init();
-	dma_memory_register_callback(DMA_MEMORY_COMPLETE_CB, M2M_DMA_complete_cb);
 #ifdef ENABLE_DBG_UART7
 	stdio_redirect_init();
 #endif
