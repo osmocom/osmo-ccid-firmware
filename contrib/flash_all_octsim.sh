3afe60200af694df96434d6e8af4320e5d2f06a2
Joachim Steiger <jsteiger@sysmocom.de>
2022-01-12 22:39:40 +0100

    add simple gangupdater for multiple octsim boards
    Change-Id: Ic10572c121ac55586ec43126b63d69529757132b

---
+++ contrib/flash_all_octsim.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# this script is to scan for octsim boards, sort them by usb path and call dfu-util on each to update the board
+# basic flow
+#  - get usb path and serialno tupel from dfu-util -l
+#  - reformat and sort by usbpath, drop the usbpath (grep cut tr sort cut)
+#  - flash the boards
+
+# TODO:
+#  - error handling
+#  - filter dfu-util output and generate nice summary
+
+dfu-util -l \
+ | grep "^Found" \
+ | grep "\[1d50:6141\]" \
+ | cut -d ' ' -f 8,13 \
+ | tr -d ',' \
+ | cut -d \" -f 2,4 \
+ | tr \" ' ' \
+ | sort \
+ | cut -d ' ' -f 2
+ | xargs -n 1 dfu-util -d 1d50:6141 -D sysmoOCTSIM.bin -R -p 
