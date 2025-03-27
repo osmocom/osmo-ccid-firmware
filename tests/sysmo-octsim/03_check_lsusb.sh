db9d29ae7c0a2a3c5effe727295f2d607bce3b64
Joachim Steiger <jsteiger@sysmocom.de>
2025-03-27 02:07:32 +0100

    fix test - filter more version strings from expected output
    Change-Id: I0f72a9722678cfd1a88190d13935b1568952b059

--- tests/sysmo-octsim/03_check_lsusb.sh
+++ tests/sysmo-octsim/03_check_lsusb.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 . ./test-data
 
-lsusb -d $USBD_TARGET -v 2>/dev/null | sed -e "s/$Bus.*Device.*:\ ID/ID/g"|sed -e "s/^.*iProduct.*2.*sysmoOCTSIM.*$/  iProduct                2 sysmoOCTSIM/g"
+lsusb -d $USBD_TARGET -v 2>/dev/null | sed -e "s/$Bus.*Device.*:\ ID/ID/g" | sed -e "s/ID\ 1d50:6141\ OpenMoko,\ Inc.\ sysmoOCTSIM.*/ID\ 1d50:6141\ OpenMoko,\ Inc.\ sysmoOCTSIM/" | sed -e "s/^.*iProduct.*2.*sysmoOCTSIM.*$/  iProduct                2 sysmoOCTSIM/g"
