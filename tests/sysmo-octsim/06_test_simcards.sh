cc5b09078a8cd3e246bcaae55eac02f78a42c812
Joachim Steiger <jsteiger@sysmocom.de>
2025-03-27 01:45:00 +0100

    expand simcard test after flashing by running pysim-shell export
    Change-Id: Iab3977c07fb41c787668b344078a0716f73b779b

--- tests/sysmo-octsim/06_test_simcards.sh
+++ tests/sysmo-octsim/06_test_simcards.sh
@@ -5,9 +5,17 @@
 sleep 1
 ./get_installed_version.sh
 
-echo "card slot 0"
+echo "card slot 0 - pysimread"
 $PYSIMREAD -p 0
 echo ""
-echo "card slot 4"
+echo "card slot 4 - pysimread"
 $PYSIMREAD -p 4
+echo ""
+echo "card slot 0 - pysimshell"
+$PYSIMSHELL -p 0 -e "export" --noprompt
+echo ""
+echo "card slot 4 - pysimshell"
+$PYSIMSHELL -p 4 -e "export" --noprompt
+
+#pySim-shell.py -p 0 -e "verify_adm" -e "export" --noprompt --csv card_data.csv
 
