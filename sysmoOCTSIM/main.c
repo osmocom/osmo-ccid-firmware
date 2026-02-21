osmocom/osmo-ccid-firmware

commit a684bc4e38b4464cf1e4a727f222abc5321a8810

Make ch9 usb tests work
This passes all tests except TD 9.9.
That's is weird, because 9.9 works fine when doing
the same commands on linux. It does not really
matter anyway, since stalling the device ep by
sending a message from the host is is only useful
for the test itself... There is no reasonable
way to continue running after random forced ep
stalls anyway, resetting the device would be the
sane option.

This adds some mildly useful debug python snippets
that can be used to inspect the device state.

Change-Id: Icadaee9d8cbe24bd3cac002cc4f710dcf846a32b

sysmoOCTSIM/main.c
@@ -38,6 +38,12 @@
 
 #include "ccid_device.h"
 #include "usb_descriptors.h"
+
+static void bdg_bkptpanic(const char *fmt, va_list args)
+{
+	__asm("BKPT #0");
+}
+
 extern struct ccid_slot_ops iso_fsm_slot_ops;
 static struct ccid_instance g_ci;
 
@@ -129,6 +135,10 @@ struct msgb *msgb_dequeue_irqsafe(struct llist_head *q)
 {
 	struct msgb *msg;
 	CRITICAL_SECTION_ENTER()
+	OSMO_ASSERT(q->next != LLIST_POISON1)
+	OSMO_ASSERT(q->next->next != LLIST_POISON1)
+	OSMO_ASSERT(q->prev != LLIST_POISON2)
+	OSMO_ASSERT(q->prev->prev != LLIST_POISON2)
 	msg = msgb_dequeue(q);
 	CRITICAL_SECTION_LEAVE()
 	return msg;
@@ -138,6 +148,10 @@ void msgb_enqueue_irqsafe(struct llist_head *q, struct msgb *msg)
 {
 	CRITICAL_SECTION_ENTER()
 	msgb_enqueue(q, msg);
+	OSMO_ASSERT(msg->list.next != LLIST_POISON1)
+	OSMO_ASSERT(msg->list.next->next != LLIST_POISON1)
+	OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
+	OSMO_ASSERT(msg->list.prev->prev != LLIST_POISON2)
 	CRITICAL_SECTION_LEAVE()
 }
 
@@ -158,6 +172,10 @@ static int submit_next_in(void)
 	ep_q->in_progress = msg;
 	rc = ccid_df_write_in(msgb_data(msg), msgb_length(msg));
 	if (rc != ERR_NONE) {
+		OSMO_ASSERT(msg->list.next != LLIST_POISON1)
+		OSMO_ASSERT(msg->list.next->next != LLIST_POISON1)
+		OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
+		OSMO_ASSERT(msg->list.prev->prev != LLIST_POISON2)
 		printf("EP %s failed: %d\r\n", ep_q->name, rc);
 		return -1;
 	}
@@ -183,6 +201,10 @@ static int submit_next_irq(void)
 	rc = ccid_df_write_irq(msgb_data(msg), msgb_length(msg));
 	/* may return HALTED/ERROR/DISABLED/BUSY/ERR_PARAM/ERR_FUNC/ERR_DENIED */
 	if (rc != ERR_NONE) {
+		// ep_q->in_progress = msg;
+		// msgb_enqueue_irqsafe(&ep_q->list, msg);
+		// OSMO_ASSERT(msg->list.next != LLIST_POISON1)
+		// OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
 		printf("EP %s failed: %d\r\n", ep_q->name, rc);
 		return -1;
 	}
@@ -195,7 +217,9 @@ static int submit_next_out(void)
 	struct msgb *msg;
 	int rc;
 
-	OSMO_ASSERT(!ep_q->in_progress);
+	if (ep_q->in_progress)
+		return 0;
+
 	msg = msgb_dequeue_irqsafe(&g_ccid_s.free_q);
 	if (!msg)
 		return -1;
@@ -206,6 +230,10 @@ static int submit_next_out(void)
 	if (rc != ERR_NONE) {
 		/* re-add to the list of free msgb's */
 		llist_add_tail_at(&msg->list, &g_ccid_s.free_q);
+		OSMO_ASSERT(msg->list.next != LLIST_POISON1)
+		OSMO_ASSERT(msg->list.next->next != LLIST_POISON1)
+		OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
+		OSMO_ASSERT(msg->list.prev->prev != LLIST_POISON2)
 		return 0;
 	}
 	return 1;
@@ -216,12 +244,32 @@ static void ccid_out_read_compl(const uint8_t ep, enum usb_xfer_code code, uint3
 {
 	struct msgb *msg = g_ccid_s.out_ep.in_progress;
 
+	/*
+	reset: resubmit from main loop when ready
+	unhalt: resubmit immediately
+	*/
+	if (code == USB_XFER_RESET || code == USB_XFER_UNHALT || code == USB_XFER_HALT) {
+		if (msg) {
+			msgb_reset(msg);
+			llist_add_tail_at(&msg->list, &g_ccid_s.free_q);
+			g_ccid_s.out_ep.in_progress = NULL;
+		}
+		if (code == USB_XFER_UNHALT) {
+			submit_next_out();
+		}
+		return;
+	}
+
 	/* add just-received msg to tail of endpoint queue */
 	OSMO_ASSERT(msg);
 	/* update msgb with the amount of data received */
 	msgb_put(msg, transferred);
 	/* append to list of pending-to-be-handed messages */
 	llist_add_tail_at(&msg->list, &g_ccid_s.out_ep.list);
+	OSMO_ASSERT(msg->list.next != LLIST_POISON1)
+	OSMO_ASSERT(msg->list.next->next != LLIST_POISON1)
+	OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
+	OSMO_ASSERT(msg->list.prev->prev != LLIST_POISON2)
 	g_ccid_s.out_ep.in_progress = NULL;
 
 	if(code != USB_XFER_DONE)
@@ -236,10 +284,18 @@ static void ccid_in_write_compl(const uint8_t ep, enum usb_xfer_code code, uint3
 {
 	struct msgb *msg = g_ccid_s.in_ep.in_progress;
 
-	OSMO_ASSERT(msg);
-	/* return the message back to the queue of free message buffers */
-	llist_add_tail_at(&msg->list, &g_ccid_s.free_q);
-	g_ccid_s.in_ep.in_progress = NULL;
+	if (msg) {
+		/* return the message back to the queue of free message buffers */
+		llist_add_tail_at(&msg->list, &g_ccid_s.free_q);
+		OSMO_ASSERT(msg->list.next != LLIST_POISON1)
+		OSMO_ASSERT(msg->list.next->next != LLIST_POISON1)
+		OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
+		OSMO_ASSERT(msg->list.prev->prev != LLIST_POISON2)
+		g_ccid_s.in_ep.in_progress = NULL;
+	}
+
+	if (code == USB_XFER_UNHALT)
+		submit_next_in();
 
 	if(code != USB_XFER_DONE)
 		return;
@@ -253,10 +309,18 @@ static void ccid_irq_write_compl(const uint8_t ep, enum usb_xfer_code code, uint
 {
 	struct msgb *msg = g_ccid_s.irq_ep.in_progress;
 
-	OSMO_ASSERT(msg);
-	/* return the message back to the queue of free message buffers */
-	llist_add_tail_at(&msg->list, &g_ccid_s.free_q);
-	g_ccid_s.irq_ep.in_progress = NULL;
+	if (msg) {
+		/* return the message back to the queue of free message buffers */
+		llist_add_tail_at(&msg->list, &g_ccid_s.free_q);
+		OSMO_ASSERT(msg->list.next != LLIST_POISON1)
+		OSMO_ASSERT(msg->list.next->next != LLIST_POISON1)
+		OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
+		OSMO_ASSERT(msg->list.prev->prev != LLIST_POISON2)
+		g_ccid_s.irq_ep.in_progress = NULL;
+	}
+
+	if (code == USB_XFER_UNHALT)
+		submit_next_irq();
 
 	if(code != USB_XFER_DONE)
 		return;
@@ -452,6 +516,10 @@ static int ccid_ops_send_in(struct ccid_instance *ci, struct msgb *msg)
 
 	/* append to list of pending-to-be-handed messages */
 	llist_add_tail_at(&msg->list, &g_ccid_s.in_ep.list);
+	OSMO_ASSERT(msg->list.next != LLIST_POISON1)
+	OSMO_ASSERT(msg->list.next->next != LLIST_POISON1)
+	OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
+	OSMO_ASSERT(msg->list.prev->prev != LLIST_POISON2)
 	submit_next_in();
 	return 0;
 }
@@ -473,21 +541,83 @@ uint8_t product_buf_descr[1 + 1 + (sizeof(product_buf) - 1) * 2];
 
 char rstcause_buf[RSTCAUSE_STR_SIZE];
 
-void do_usb_res(void)
+void reset_all_stuff_irq(void)
 {
-	hri_usbdevice_set_CTRLA_SWRST_bit(USB);
-	uint32_t *application_start_address = (uint32_t *)(16384);
-	__set_MSP(*application_start_address);
-	__DSB();
-	__ISB();
-	asm("bx %0" ::"r"(*(application_start_address + 1)));
+	SysTick->CTRL = 0; // no clock ticks for osmo timers
+
+	Sercom *const sercom_modules[] = SERCOM_INSTS;
+	for (uint32_t i = 0; i < SERCOM_INST_NUM; i++) {
+		hri_sercomusart_clear_CTRLA_ENABLE_bit(sercom_modules[i]);
+		hri_sercomusart_clear_INTFLAG_reg(sercom_modules[i], 0xff);
+	}
 }
 
-extern volatile bool delayed_usb_reset;
+extern volatile bool was_unconfigured_flag;
+
+void reset_all_stuff_non_irq(void)
+{
+	if (!was_unconfigured_flag)
+		return;
+
+	CRITICAL_SECTION_ENTER()
+
+	for (int i = 0; i <= usb_fs_descs.ccid.class.bMaxSlotIndex; i++) {
+		g_ci.slot_ops->handle_fsm_events(&g_ci.slot[i], true);
+	}
+
+	for (int i = 0; i <= usb_fs_descs.ccid.class.bMaxSlotIndex; i++) {
+		g_ci.slot_ops->icc_set_insertion_status(&g_ci.slot[i], false);
+		g_ci.slot_ops->handle_fsm_events(&g_ci.slot[i], true);
+	}
+
+	for (int i = 0; i <= usb_fs_descs.ccid.class.bMaxSlotIndex; i++) {
+		g_ci.slot_ops->handle_fsm_events(&g_ci.slot[i], true);
+	}
+
+	volatile struct usb_ep_q *all_epqs[] = { &g_ccid_s.in_ep, &g_ccid_s.irq_ep, &g_ccid_s.out_ep };
+	for (int i = 0; i < ARRAY_SIZE(all_epqs); i++) {
+		volatile struct usb_ep_q *cur_epq = all_epqs[i];
+		struct msgb *msg;
+		while ((msg = msgb_dequeue_irqsafe(&cur_epq->list))) {
+			msgb_reset(msg);
+			llist_add_tail_at(&msg->list, &g_ccid_s.free_q);
+			OSMO_ASSERT(msg->list.next != LLIST_POISON1)
+			OSMO_ASSERT(msg->list.next->next != LLIST_POISON1)
+			OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
+			OSMO_ASSERT(msg->list.prev->prev != LLIST_POISON2)
+		}
+#if 0
+		struct msgb *cur_msg = cur_epq->in_progress;
+		if (cur_msg) {
+			/*
+			if (i == 2 && usb_d_ep_get_status(/*_ccid_df_funcd.func_ep_out*/ 2, 0) == USB_BUSY)
+			 	continue;
+			*/
+
+			msgb_reset(cur_msg);
+			llist_add_tail_at(&cur_msg->list, &g_ccid_s.free_q);
+			OSMO_ASSERT(msg->list.next != LLIST_POISON1)
+			OSMO_ASSERT(msg->list.next->next != LLIST_POISON1)
+			OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
+			OSMO_ASSERT(msg->list.prev->prev != LLIST_POISON2)
+			cur_epq->in_progress = NULL;
+		}
+#endif
+	}
+
+	SysTick->CTRL = 1;
+
+	// while (!ccid_df_is_enabled())
+	// 	;
+	g_ccid_s.card_insert_mask = 0;
+	was_unconfigured_flag = false;
+	CRITICAL_SECTION_LEAVE()
+	ccid_eps_enable();
+}
 
 int main(void)
 {
-
+	osmo_set_panic_handler(&bdg_bkptpanic);
 #if 0
 CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk ; //| /* tracing*/
 ////CoreDebug_DEMCR_MON_EN_Msk; /* mon interupt catcher */
@@ -567,9 +697,10 @@ DWT->FUNCTION1 =    (0b10 << DWT_FUNCTION_DATAVSIZE_Pos) |  /* DATAVSIZE 10 - dw
 		struct msgb *msg = msgb_alloc(300, "ccid");
 		OSMO_ASSERT(msg);
 		/* return the message back to the queue of free message buffers */
+		OSMO_ASSERT(msg->list.next != LLIST_POISON1)
 		llist_add_tail_at(&msg->list, &g_ccid_s.free_q);
 	}
-	submit_next_out();
+	// submit_next_out();
 	CRITICAL_SECTION_LEAVE()
 #if 0
 	/* CAN_RX */
@@ -585,8 +716,10 @@ DWT->FUNCTION1 =    (0b10 << DWT_FUNCTION_DATAVSIZE_Pos) |  /* DATAVSIZE 10 - dw
 
 //	command_print_prompt();
 	while (true) { // main loop
-		if (delayed_usb_reset)
-			do_usb_res();
+		if (was_unconfigured_flag) {
+			reset_all_stuff_non_irq();
+			submit_next_out();
+		}
 		poll_extpower_detect();
 		command_try_recv();
 		poll_card_detect();
@@ -597,19 +730,18 @@ DWT->FUNCTION1 =    (0b10 << DWT_FUNCTION_DATAVSIZE_Pos) |  /* DATAVSIZE 10 - dw
 		feed_ccid();
 		osmo_timers_update();
 		int qs = llist_count_at(&g_ccid_s.free_q);
-		if(qs > NUM_OUT_BUF)
-			for (int i= 0; i < qs-NUM_OUT_BUF; i++){
+		if (qs > NUM_OUT_BUF)
+			for (int i = 0; i < qs - NUM_OUT_BUF; i++) {
 				struct msgb *msg = msgb_dequeue_irqsafe(&g_ccid_s.free_q);
 				if (msg)
-				msgb_free(msg);
+					msgb_free(msg);
 			}
-		if(qs < NUM_OUT_BUF)
-			for (int i= 0; i < NUM_OUT_BUF-qs; i++){
-				struct msgb *msg = msgb_alloc(300,"ccid");
+		if (qs < NUM_OUT_BUF)
+			for (int i = 0; i < NUM_OUT_BUF - qs; i++) {
+				struct msgb *msg = msgb_alloc(300, "ccid");
 				OSMO_ASSERT(msg);
 				/* return the message back to the queue of free message buffers */
 				llist_add_tail_at(&msg->list, &g_ccid_s.free_q);
 			}
-
 	}
 }

ccid_common/cuart.c
@@ -154,6 +154,7 @@ int card_uart_ctrl(struct card_uart *cuart, enum card_uart_ctl ctl, int arg)
 		/* we have to reset this somewhere, and powering down loses all state
 		 * this is not hw specific so it belongs here, after handling the hw specific part */
 		if (!arg) {
+			osmo_timer_del(&cuart->wtime_tmr);
 			cuart->tx_busy = false;
 			cuart->rx_threshold = 1;
 			cuart->wtime_etu = 9600; /* ISO 7816-3 Section 8.1 */

contrib/ch9_epstall.py
@@ -1,0 +1,209 @@
+#!/usr/bin/env python3
+"""
+Copyright 2025 sysmocom - s.f.m.c. GmbH <info@sysmocom.de>
+
+SPDX-License-Identifier: 0BSD
+
+Permission to use, copy, modify, and/or distribute this software for any purpose
+with or without fee is hereby granted.THE SOFTWARE IS PROVIDED "AS IS" AND THE
+AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR
+BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE
+USE OR PERFORMANCE OF THIS SOFTWARE.
+
+"""
+
+"""
+ch9 9.9 usb ep halt test code snippets
+"""
+
+import sys
+import getopt
+import ctypes
+import libusb
+
+VENDOR = 0x1d50
+PRODUCT = 0x6141
+TIMEOUT = 1000  # milliseconds
+
+def is_myusb(dev):
+    desc = libusb.device_descriptor()
+    ret = libusb.get_device_descriptor(dev, ctypes.byref(desc))
+    if ret != 0:
+        sys.stderr.write("Failed to get device descriptor\n")
+        return False
+    return (desc.idVendor == VENDOR and desc.idProduct == PRODUCT)
+
+
+def print_device_endpoints(dev):
+    """Prints the list of endpoints for each interface from the first configuration."""
+    enum_mapping = {
+        value: name
+        for name, value in vars(libusb).items()
+        if name.startswith("LIBUSB_ENDPOINT_TRANSFER_TYPE_") and isinstance(value, int)
+    }
+
+    def _dir(ep):
+        return "IN" if (ep & libusb.LIBUSB_ENDPOINT_DIR_MASK) == libusb.LIBUSB_ENDPOINT_DIR_MASK else "OUT"
+
+    cfg_ptr = ctypes.POINTER(libusb.config_descriptor)()
+    ret = libusb.get_config_descriptor(dev, 0, ctypes.byref(cfg_ptr))
+    if ret != 0:
+        sys.stderr.write("Error getting config descriptor: error {}\n".format(ret))
+        return
+    cfg = cfg_ptr.contents
+    print("Device Configuration:")
+    print("  Number of interfaces: {}".format(cfg.bNumInterfaces))
+    # Iterate over interfaces
+    for i in range(cfg.bNumInterfaces):
+        iface = cfg.interface[i]
+        print(" Interface {}: {} alternate setting(s)".format(i, iface.num_altsetting))
+        for j in range(iface.num_altsetting):
+            alt = iface.altsetting[j]
+            print("  Interface Altsetting {}: Interface Number {}, Number of Endpoints: {}" .format(j, alt.bInterfaceNumber, alt.bNumEndpoints))
+            for k in range(alt.bNumEndpoints):
+                ep = alt.endpoint[k]
+                print(f"    Endpoint {k}: address: 0x{ep.bEndpointAddress:02x} {_dir(ep.bEndpointAddress):3} {enum_mapping[ep.bmAttributes & 0b11]}")
+    libusb.free_config_descriptor(cfg_ptr)
+
+
+def set_endpoint_halt(handle, ep, timeout=TIMEOUT):
+    # bmRequestType: 0x02 (Host-to-device, Standard, Endpoint)
+    # bRequest:      0x03 (SET_FEATURE) with wValue = 0 (ENDPOINT_HALT)
+    ret = libusb.control_transfer(handle, 0x02, 0x03, 0, ep, None, 0, timeout)
+    return ret
+
+
+def get_endpoint_halt(handle, ep, timeout=TIMEOUT):
+    # bmRequestType: 0x82 (Device-to-host, Standard, Endpoint)
+    # bRequest:      0x00 (GET_STATUS) with 2-byte response.
+    buf = (ctypes.c_ubyte * 2)()
+    ret = libusb.control_transfer(handle, 0x82, 0x00, 0, ep, buf, 2, timeout)
+    if ret < 0:
+        return None, ret
+    status = buf[0] | (buf[1] << 8)
+    halted = (status & 1) != 0
+    return halted, 0
+
+
+def clear_endpoint_halt(handle, ep, timeout=TIMEOUT):
+    # bmRequestType: 0x02 (Host-to-device, Standard, Endpoint)
+    # bRequest:      0x01 (CLEAR_FEATURE) with wValue = 0 (ENDPOINT_HALT)
+    # ret = libusb.control_transfer(handle, 0x02, 0x01, 0, ep, None, 0, timeout)
+
+    # use proper clear, because the halt resets the toggle, so the host needs to know
+    ret = libusb.clear_halt(handle, ep)
+    return ret
+
+
+def main():
+    try:
+        opts, _ = getopt.getopt(sys.argv[1:], "e:d:i:")
+    except getopt.GetoptError as err:
+        sys.stderr.write("ERR: Invalid options: {}\n".format(err))
+        sys.exit(1)
+
+    ep = None
+    iface = 0
+    for opt, arg in opts:
+        if opt == '-e':
+            ep = int(arg, 0)
+        elif opt == '-i':
+            iface = int(arg, 0)
+
+    ctx = ctypes.POINTER(libusb.context)()
+    ret = libusb.init(ctypes.byref(ctx))
+    if ret != 0:
+        sys.stderr.write("Failed to initialize libusb, error {}\n".format(ret))
+        sys.exit(1)
+
+    # Get device list.
+    devs = ctypes.POINTER(ctypes.POINTER(libusb.device))()
+    cnt = libusb.get_device_list(ctx, ctypes.byref(devs))
+    print("Number of attached USB devices = {}".format(cnt))
+
+    my_dev = None
+    for i in range(cnt):
+        dev = devs[i]
+        if is_myusb(dev):
+            my_dev = dev
+            break
+
+    if not my_dev:
+        sys.stderr.write("ERR: Device not found\n")
+        libusb.free_device_list(devs, 1)
+        libusb.exit(ctx)
+        sys.exit(1)
+
+    # Print the list of endpoints before performing endpoint halt operations.
+    print("Listing device endpoints:")
+    print_device_endpoints(my_dev)
+
+    if ep is None:
+        sys.stderr.write("ERR: -e: Specify endpoint number\n")
+        libusb.free_device_list(devs, 1)
+        libusb.exit(ctx)
+        sys.exit(1)
+
+    handle = ctypes.POINTER(libusb.device_handle)()
+    ret = libusb.open(my_dev, ctypes.byref(handle))
+    if ret != 0:
+        sys.stderr.write("ERR: Failed to open device, error {}\n".format(libusb.strerror(ret)))
+        libusb.free_device_list(devs, 1)
+        libusb.exit(ctx)
+        sys.exit(1)
+
+    if libusb.kernel_driver_active(handle, iface) == 1:
+        libusb.detach_kernel_driver(handle, iface)
+
+    ret = libusb.claim_interface(handle, iface)
+    if ret != 0:
+        sys.stderr.write("ERR: Failed to claim interface {}: error {}\n".format(iface, libusb.strerror(ret)))
+        libusb.close(handle)
+        libusb.free_device_list(devs, 1)
+        libusb.exit(ctx)
+        sys.exit(1)
+
+    print("Performing halt operations on endpoint 0x{:02x}".format(ep))
+
+    # Set endpoint halt (stall).
+    ret = set_endpoint_halt(handle, ep)
+    if ret < 0:
+        sys.stderr.write("Error setting halt on endpoint 0x{:02x}, error {}\n".format(ep, libusb.strerror(ret)))
+    else:
+        print("Endpoint 0x{:02x} set to halted state.".format(ep))
+
+    # Get the halt status.
+    halted, ret = get_endpoint_halt(handle, ep)
+    if ret < 0:
+        sys.stderr.write("Error getting halt status on endpoint 0x{:02x}, error {}\n".format(ep, libusb.strerror(ret)))
+    else:
+        print("Endpoint 0x{:02x} halt status: {}".format(ep, halted))
+
+    # Clear the endpoint halt.
+    ret = clear_endpoint_halt(handle, ep)
+    if ret < 0:
+        sys.stderr.write("Error clearing halt on endpoint 0x{:02x}, error {}\n".format(ep, libusb.strerror(ret)))
+    else:
+        print("Clear halt command issued for endpoint 0x{:02x}.".format(ep))
+
+    # Verify the status after clearing.
+    halted, ret = get_endpoint_halt(handle, ep)
+    if ret < 0:
+        sys.stderr.write("Error getting halt status after clear on endpoint 0x{:02x}, error {}\n".format(ep, libusb.strerror(ret)))
+    else:
+        print("Endpoint 0x{:02x} halt status after clear: {}".format(ep, halted))
+
+    # Cleanup.
+    libusb.release_interface(handle, iface)
+    libusb.attach_kernel_driver(handle, iface)
+    libusb.close(handle)
+    libusb.free_device_list(devs, 1)
+    libusb.exit(ctx)
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()

commit 88eabc156a6177908dbe0c0dd11f2ef3219a3b47

main: don't access msgbs after giving them away
Because of IRQ, it is dangerous to access a msgb,
which has been giving away to a queue.

Further OSMO_ASSERT() should never fail on a queue'd object,
except it has been taken already by the IRQ.
Fix a potential crash.

Fixes: a684bc4e38b4 ("Make ch9 usb tests work")
Change-Id: I79844f77d5cd75e08b0eb44b22c4ad223cb79dcb

sysmoOCTSIM/main.c
@@ -199,8 +199,6 @@ static int submit_next_irq(void)
 	if (rc != ERR_NONE) {
 		// ep_q->in_progress = msg;
 		// msgb_enqueue_irqsafe(&ep_q->list, msg);
-		// OSMO_ASSERT(msg->list.next != LLIST_POISON1)
-		// OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
 		printf("EP %s failed: %d\r\n", ep_q->name, rc);
 		return -1;
 	}
@@ -226,10 +224,6 @@ static int submit_next_out(void)
 	if (rc != ERR_NONE) {
 		/* re-add to the list of free msgb's */
 		llist_add_tail_at(&msg->list, &g_ccid_s.free_q);
-		OSMO_ASSERT(msg->list.next != LLIST_POISON1)
-		OSMO_ASSERT(msg->list.next->next != LLIST_POISON1)
-		OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
-		OSMO_ASSERT(msg->list.prev->prev != LLIST_POISON2)
 		return 0;
 	}
 	return 1;
@@ -262,10 +256,6 @@ static void ccid_out_read_compl(const uint8_t ep, enum usb_xfer_code code, uint3
 	msgb_put(msg, transferred);
 	/* append to list of pending-to-be-handed messages */
 	llist_add_tail_at(&msg->list, &g_ccid_s.out_ep.list);
-	OSMO_ASSERT(msg->list.next != LLIST_POISON1)
-	OSMO_ASSERT(msg->list.next->next != LLIST_POISON1)
-	OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
-	OSMO_ASSERT(msg->list.prev->prev != LLIST_POISON2)
 	g_ccid_s.out_ep.in_progress = NULL;
 
 	if(code != USB_XFER_DONE)
@@ -283,10 +273,6 @@ static void ccid_in_write_compl(const uint8_t ep, enum usb_xfer_code code, uint3
 	if (msg) {
 		/* return the message back to the queue of free message buffers */
 		llist_add_tail_at(&msg->list, &g_ccid_s.free_q);
-		OSMO_ASSERT(msg->list.next != LLIST_POISON1)
-		OSMO_ASSERT(msg->list.next->next != LLIST_POISON1)
-		OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
-		OSMO_ASSERT(msg->list.prev->prev != LLIST_POISON2)
 		g_ccid_s.in_ep.in_progress = NULL;
 	}
 
@@ -308,10 +294,6 @@ static void ccid_irq_write_compl(const uint8_t ep, enum usb_xfer_code code, uint
 	if (msg) {
 		/* return the message back to the queue of free message buffers */
 		llist_add_tail_at(&msg->list, &g_ccid_s.free_q);
-		OSMO_ASSERT(msg->list.next != LLIST_POISON1)
-		OSMO_ASSERT(msg->list.next->next != LLIST_POISON1)
-		OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
-		OSMO_ASSERT(msg->list.prev->prev != LLIST_POISON2)
 		g_ccid_s.irq_ep.in_progress = NULL;
 	}
 
@@ -513,10 +495,6 @@ static int ccid_ops_send_in(struct ccid_instance *ci, struct msgb *msg)
 
 	/* append to list of pending-to-be-handed messages */
 	llist_add_tail_at(&msg->list, &g_ccid_s.in_ep.list);
-	OSMO_ASSERT(msg->list.next != LLIST_POISON1)
-	OSMO_ASSERT(msg->list.next->next != LLIST_POISON1)
-	OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
-	OSMO_ASSERT(msg->list.prev->prev != LLIST_POISON2)
 	submit_next_in();
 	return 0;
 }
@@ -578,10 +556,6 @@ void reset_all_stuff_non_irq(void)
 		while ((msg = msgb_dequeue_irqsafe(&cur_epq->list))) {
 			msgb_reset(msg);
 			llist_add_tail_at(&msg->list, &g_ccid_s.free_q);
-			OSMO_ASSERT(msg->list.next != LLIST_POISON1)
-			OSMO_ASSERT(msg->list.next->next != LLIST_POISON1)
-			OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
-			OSMO_ASSERT(msg->list.prev->prev != LLIST_POISON2)
 		}
 #if 0
 		struct msgb *cur_msg = cur_epq->in_progress;
@@ -593,10 +567,6 @@ void reset_all_stuff_non_irq(void)
 
 			msgb_reset(cur_msg);
 			llist_add_tail_at(&cur_msg->list, &g_ccid_s.free_q);
-			OSMO_ASSERT(msg->list.next != LLIST_POISON1)
-			OSMO_ASSERT(msg->list.next->next != LLIST_POISON1)
-			OSMO_ASSERT(msg->list.prev != LLIST_POISON2)
-			OSMO_ASSERT(msg->list.prev->prev != LLIST_POISON2)
 			cur_epq->in_progress = NULL;
 		}
 #endif