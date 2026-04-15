45e138d92d6dc5aa1c7f262bc7228da3565b4cce
Oliver Smith <osmith@sysmocom.de>
2026-04-15 12:23:56 +0200

    tests: use last known working version
    Related: https://jenkins.osmocom.org/jenkins/job/octsim_osmo-ccid-firmware-old/label=octsimtest/4436/
    Change-Id: Ib0e27919b7ca78025b8c3b8ed4ae29559fe051e4

--- tests/sysmo-octsim/fetch_image.sh
+++ tests/sysmo-octsim/fetch_image.sh
@@ -2,5 +2,5 @@
 . ./test-data
 
 echo "fetch image"
-wget -O $DFU_IMAGE http://ftp.osmocom.org/binaries/osmo-ccid-firmware/latest/sysmoOCTSIM.bin -nv
+wget -O $DFU_IMAGE https://downloads.osmocom.org/binaries/osmo-ccid-firmware/all/sysmoOCTSIM-0.3.44-639d.bin -nv
 echo "done"
