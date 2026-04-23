07c6b107d33d147baaa7b0a4fceb7139dad0a5d7
Oliver Smith <osmith@sysmocom.de>
2026-04-23 12:57:21 +0200

    tests: use pysim-shell instead of pysim-read
    Replace pysim-read commands with pysim-shell commands, so we can locally
    reproduce runs with modern cards like the c2t euicc which is not
    supported by pysim-read.
    
    Fixes: SYS#8014
    Change-Id: I6fcb616c18722d5c324d1992ba6490ddc0295715

--- tests/sysmo-octsim/06_test_simcards.sh
+++ tests/sysmo-octsim/06_test_simcards.sh
@@ -5,16 +5,16 @@
 sleep 1
 ./get_installed_version.sh
 
-echo "card slot 0 - pysimread"
-$PYSIMREAD -p 0
+echo "card slot 0 - pysimshell (read)"
+$PYSIMSHELL -p 0 --noprompt --script 06_test_simcards_toaster.script
 echo ""
-echo "card slot 4 - pysimread"
-$PYSIMREAD -p 4
+echo "card slot 4 - pysimshell (read)"
+$PYSIMSHELL -p 4 --noprompt --script 06_test_simcards_toaster.script
 echo ""
-echo "card slot 0 - pysimshell"
+echo "card slot 0 - pysimshell (export)"
 $PYSIMSHELL -p 0 -e "export" --noprompt
 echo ""
-echo "card slot 4 - pysimshell"
+echo "card slot 4 - pysimshell (export)"
 $PYSIMSHELL -p 4 -e "export" --noprompt
 
 #pySim-shell.py -p 0 -e "verify_adm" -e "export" --noprompt --csv card_data.csv
